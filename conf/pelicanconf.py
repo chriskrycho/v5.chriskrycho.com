26e3d8425ad0ad50593c5091706ae1c64ee9d9ad
Chris Krycho <chris@chriskrycho.com>
2013-12-27 14:17:18 -0500

    Re-commit configuration files.
    --HG--
    branch : 2013_dev

---
+++ conf/pelicanconf.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'Chris Krycho'
+SITENAME = 'chriskrycho.com'
+SITEURL = ''
+
+TIMEZONE = 'America/New_York'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+
+# Social widget
+SOCIAL = (('Twitter', 'http://twitter.com/chriskrycho'),
+          ('App.net', 'http://app.net/chriskrycho'),
+          ('Facebook', 'http://www.facebook.com/chriskrycho'),)
+
+DEFAULT_PAGINATION = 5
+
+# Category settings
+USE_FOLDER_AS_CATEGORY = True  # note: this is the default
+DEFAULT_CATEGORY = 'blog'
+
+# Output
+OUTPUT_SOURCES = True
+OUTPUT_SOURCES_EXTENSION = ".txt"
+
+# Markdown
+MD_EXTENSIONS = ['codehilite(css_class=highlight)', 'extra', 'toc', 'headerid']
+
+# Uncomment following line if you want document-relative URLs when developing
+RELATIVE_URLS = True
