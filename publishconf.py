a94ec15fb296123fdf51d8512a04c3713bc9bd09
Christopher Krycho <chris@chriskrycho.com>
2014-01-29 17:19:04 -0500

    Add protocol to publish configuration.

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = '//chriskrycho.com'
+SITEURL = 'http://chriskrycho.com'
 RELATIVE_URLS = False
 
 FEED_ALL_RSS = 'feed.xml'
