6c7b8807a24df32be948a2406b94def2f0ea846d
Christopher Krycho <chris@chriskrycho.com>
2014-02-21 12:58:08 -0500

    Update publishconf.py to reflect hosting on GitHub (for #16).

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://chriskrycho.com'
+SITEURL = 'http://www.chriskrycho.com'
 RELATIVE_URLS = False
 
 FEED_ALL_RSS = 'feed.xml'
