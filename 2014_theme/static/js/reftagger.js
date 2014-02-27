85a9c866eed68556ce4b2214ab2e8e4d937e683f
Christopher Krycho <chris@chriskrycho.com>
2014-02-26 20:11:26 -0500

    Add latest theme work.

---
+++ 2014_theme/static/js/reftagger.js
@@ -0,0 +1,10 @@
+var refTagger = {
+    settings: {
+        bibleVersion: "ESV"
+    }
+};
+(function (d, t) {
+    var g = d.createElement(t), s = d.getElementsByTagName(t)[0];
+    g.src = "//api.reftagger.com/v2/RefTagger.js";
+    s.parentNode.insertBefore(g, s);
+}(document, "script"));
