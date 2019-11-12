81f76140c3db07ef5d0278a1b40d610f14e4437c
Chris Krycho <hello@chriskrycho.com>
2019-11-12 09:06:37 -0700

    Improve docs in spacewell.d.ts.

--- lib/spacewell.d.ts
+++ lib/spacewell.d.ts
@@ -4,6 +4,12 @@ export interface Options {
    initials?: boolean
 }
 
+/**
+   Given a valid DOM element `container`, apply nice typographical spacing.
+
+   @param options Options for which spacing rules to use.
+   @param [content] A document element to apply rules to.
+ */
 function spacewell(options: Options): (content: string) => string
 function spacewell(options: Options, content: string): string
 function spacewell(
