1fcde96362b14e2579db248f93dea33123a39eb7
Chris Krycho <hello@chriskrycho.com>
2019-11-17 13:15:54 -0700

    Change the default `localeDate` format
    On viewing the site in its more fleshed-out state, I realized that the
    day of the week was irrelevant and basically just noise, so I removed
    it from the default.

--- eleventy/locale-date.ts
+++ eleventy/locale-date.ts
@@ -3,7 +3,7 @@ import toDateTime from './to-date-time'
 
 const OPTIONS = { zone: 'America/Denver' }
 
-export const localeDate = (date: Date | string, format = 'DDDD'): string =>
+export const localeDate = (date: Date | string, format = 'DDD'): string =>
    (typeof date === 'string'
       ? toDateTime(date)
       : DateTime.fromJSDate(date, OPTIONS)
