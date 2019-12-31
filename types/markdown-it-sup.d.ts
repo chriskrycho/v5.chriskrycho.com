b6495e0935ec3f8ad294014a41dc0621af6eddce
Chris Krycho <hello@chriskrycho.com>
2019-12-31 09:27:17 -0700

    Fix header for markdown-it-sup type defs

--- types/markdown-it-sup.d.ts
+++ types/markdown-it-sup.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for MarkdownItDeflist (markdown-it-deflist) 4.0
+// Type definitions for MarkdownItSup (markdown-it-sup) 1.0
 // Project: https://github.com/markdown-it/markdown-it-sup
 // Definitions by: Chris Krycho <https://github.com/chriskrycho>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
