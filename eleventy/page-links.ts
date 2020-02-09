22bf9084923fc22cbce36545a193aa299b0f7f1b
Chris Krycho <hello@chriskrycho.com>
2020-02-08 19:03:02 -0700

    Fix lint errors in `page-links` module

--- eleventy/page-links.ts
+++ eleventy/page-links.ts
@@ -1,10 +1,13 @@
 import SiteConfig from '../site/_data/config'
 
 /** Link to the history of the file on GitHub */
-export const history = (path: string) => `${SiteConfig.repo}/commits/master/${path}`
+export const history = (path: string): string =>
+   `${SiteConfig.repo}/commits/master/${path}`
 
 /** Link to edit the file on GitHub */
-export const edit = (inputPath: string) => `${SiteConfig.repo}/edit/master/${inputPath}`
+export const edit = (inputPath: string): string =>
+   `${SiteConfig.repo}/edit/master/${inputPath}`
 
 /** Link to view the file on GitHub */
-export const source = (inputPath: string) => `${SiteConfig.repo}/blob/master/${inputPath}`
+export const source = (inputPath: string): string =>
+   `${SiteConfig.repo}/blob/master/${inputPath}`
