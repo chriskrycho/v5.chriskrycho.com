833c7776c1b66b68cc9c1acddf964c3a67d98e01
Chris Krycho <hello@chriskrycho.com>
2020-04-28 21:20:47 -0600

    This probably won't help either.

--- eleventy/date-time.ts
+++ eleventy/date-time.ts
@@ -1,28 +1,27 @@
 import { DateTime, DateTimeOptions } from 'luxon'
-import { Maybe } from 'true-myth'
 
 type Parse = (text: string, options?: DateTimeOptions | undefined) => DateTime
 
-const maybeDateTime = (parse: Parse, input: string): Maybe<DateTime> => {
+const maybeDateTime = (parse: Parse, input: string): DateTime | null => {
    const parsed = parse(input)
-   return parsed.isValid ? Maybe.just(parsed) : Maybe.nothing()
+   return parsed.isValid ? parsed : null
 }
 
 const TZ = { zone: 'America/Denver' }
 
 // Same parsing rules as 11ty itself uses: ISO or SQL, nothing else.
 export const toDateTime = (input: string): DateTime => {
-   return maybeDateTime(s => DateTime.fromISO(s, TZ), input)
-      .or(maybeDateTime(s => DateTime.fromSQL(s, TZ), input))
-      .unwrapOrElse(() => {
-         throw new Error(`Could not parse date: ${input}`)
-      })
+   const dateTime =
+      maybeDateTime(s => DateTime.fromISO(s, TZ), input) ??
+      maybeDateTime(s => DateTime.fromSQL(s, TZ), input)
+   if (!dateTime) throw new Error(`Could not parse date: ${input}`)
+   return dateTime
 }
 
 export const canParseDate = (date: unknown): date is string | Date =>
    typeof date === 'string' || date instanceof Date
 
 export const fromDateOrString = (date: Date | string): DateTime =>
-   typeof date === 'string' ? toDateTime(date) : DateTime.fromJSDate(date)
+   typeof date === 'string' ? toDateTime(date) : DateTime.fromJSDate(date, TZ)
 
 export default toDateTime
