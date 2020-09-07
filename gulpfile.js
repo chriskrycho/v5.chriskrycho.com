712bdf1e943b0179b0e57fae332e082eaf0c2dea
Chris Krycho <hello@chriskrycho.com>
2020-09-07 17:12:48 -0600

    Style: add reasonably-decent print stylesheet
    Resolves #99

--- gulpfile.js
+++ gulpfile.js
@@ -16,24 +16,32 @@ function style() {
    return build('./site/_includes/styles/style.scss')()
 }
 
+function print() {
+   return build('./site/_includes/styles/print.scss')()
+}
+
 function fonts() {
    return build('./site/_includes/styles/fonts.scss')()
 }
 
 function clean() {
-   return del(['./site/_includes/styles/style.css', './site/_includes/styles/fonts.css'])
+   return del([
+      './site/_includes/styles/style.css',
+      './site/_includes/styles/fonts.css',
+      './site/_includes/styles/print.css',
+   ])
 }
 
-const all = parallel(style, fonts)
+const all = parallel(style, fonts, print)
 
 function watchStyles() {
    watch('./site/_includes/styles/**/*.scss', all)
 }
-// const watchStyles = () => watch('./site/_includes/styles/**/*.scss', all)
 
 exports.clean = clean
 exports.style = style
 exports.fonts = fonts
+exports.print = print
 exports.watch = watchStyles
 exports.all = all
 exports.default = series(clean, all)
