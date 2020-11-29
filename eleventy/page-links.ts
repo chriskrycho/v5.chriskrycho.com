4d9a935e6b5c60795feadfd3b58f1c937daab4a7
Chris Krycho <hello@chriskrycho.com>
2020-11-29 10:02:42 -0700

    Eliminate needless leading './' from edit, history links

--- eleventy/page-links.ts
+++ eleventy/page-links.ts
@@ -1,13 +1,15 @@
 import SiteConfig from '../site/_data/config'
 
+const corrected = (path: string): string => path.replace(/^\.\//, '')
+
 /** Link to the history of the file on GitHub */
 export const history = (path: string): string =>
-   `${SiteConfig.repo}/commits/master/${path}`
+   `${SiteConfig.repo}/commits/master/${corrected(path)}`
 
 /** Link to edit the file on GitHub */
 export const edit = (inputPath: string): string =>
-   `${SiteConfig.repo}/edit/master/${inputPath}`
+   `${SiteConfig.repo}/edit/master/${corrected(inputPath)}`
 
 /** Link to view the file on GitHub */
 export const source = (inputPath: string): string =>
-   `${SiteConfig.repo}/blob/master/${inputPath}`
+   `${SiteConfig.repo}/blob/master/${corrected(inputPath)}`
