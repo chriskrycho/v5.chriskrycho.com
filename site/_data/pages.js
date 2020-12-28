ee28f04652b52a022817686143bd10f2fb212990
Chris Krycho <hello@chriskrycho.com>
2020-12-28 08:19:54 -0700

    Essays: realize 'Private Chat' was one

--- site/_data/pages.js
+++ site/_data/pages.js
@@ -1,11 +1,10 @@
 module.exports = [
    { type: 'page', title: 'Archive', path: '/archive/' },
    { type: 'separator' },
-   // { type: 'page', title: 'Essays', path: '/essays/' },
+   { type: 'page', title: 'Essays', path: '/essays/' },
    { type: 'page', title: 'Journal', path: '/journal/' },
    { type: 'page', title: 'Library', path: '/library/' },
    { type: 'page', title: 'Notes', path: '/notes/' },
-   // { type: 'page', title: 'Longform', path: '#' },
    // { type: 'separator' },
    // { type: 'page', title: 'Photos', path: '#' },
    { type: 'page', title: 'Elsewhere', path: '/elsewhere/' },
