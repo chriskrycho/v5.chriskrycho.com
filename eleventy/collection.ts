1766df4315d6c500975264caba3766cebef5f50c
Chris Krycho <hello@chriskrycho.com>
2021-07-18 13:44:11 -0600

    Support 'ghost' collections
    This allows me to use folders as 'collections' by default, while opting
    out for organizational-only collections.
    
    (Thinking about `lx` design: should this be the default? Or should it
    be the other way: folders only become 'collections' when explicitly
    opting in? Worth playing with both ways, but given that I only need to
    do it in a couple places in the current design, opt-*out* seems like
    it's probably a reasonable starting point.)

--- eleventy/collection.ts
+++ eleventy/collection.ts
@@ -8,23 +8,31 @@ export const collectionName = ({ from: pathString }: { from: string }): string =
    pathString.split(path.sep).filter(excluded).join('__');
 
 /** Get the collection corresponding to a given path slug */
-export const toCollection = (slug: string): string =>
+const toCollection = (slug: string): string =>
    collectionName({ from: path.dirname(slug.trim()) });
 
-export const toCollectionUrl = (slug: string): string =>
-   toCollection(slug)
-      .split('__')
-      .map(
-         (subPaths) =>
-            subPaths
-               .split(' ') // "A B C" -> ["A", "B", "C"]
-               .map((w) => w.toLowerCase()) // -> ["a", "b", "c"]
-               .join('-'), // -> "a-b-c"
-      )
-      .join('/');
+export const toCollectionUrl = (
+   slug: string,
+   isGhostCollection?: boolean,
+): string | undefined =>
+   isGhostCollection
+      ? toRootCollection(slug)
+      : toCollection(slug)
+           .split('__')
+           .map(
+              (subPaths) =>
+                 subPaths
+                    .split(' ') // "A B C" -> ["A", "B", "C"]
+                    .map((w) => w.toLowerCase()) // -> ["a", "b", "c"]
+                    .join('-'), // -> "a-b-c"
+           )
+           .join('/');
 
-export const toCollectionName = (slug: string): string =>
-   toCollection(slug).replace(/__/g, ': ');
+export const toCollectionName = (
+   slug: string,
+   isGhostCollection?: boolean,
+): string | undefined =>
+   isGhostCollection ? toRootCollection(slug) : toCollection(slug).replace(/__/g, ': ');
 
 export const toRootCollection = (slug: string): string | undefined =>
    collectionName({ from: path.dirname(slug.trim()) }).split('__')[0];
