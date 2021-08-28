7e7bf3383f40228ec0435c8fee39d862953b71b8
Chris Krycho <hello@chriskrycho.com>
2021-08-27 19:09:47 -0600

    Fix link generation (urlencode!)

--- eleventy/page-links.ts
+++ eleventy/page-links.ts
@@ -2,14 +2,16 @@ import SiteConfig from '../site/_data/config';
 
 const corrected = (path: string): string => path.replace(/^\.\//, '');
 
+const renderable = (path: string) => encodeURIComponent(corrected(path));
+
 /** Link to the history of the file on GitHub */
 export const history = (path: string): string =>
-   `${SiteConfig.repo}/commits/main/${corrected(path)}`;
+   `${SiteConfig.repo}/commits/main/${renderable(path)}`;
 
 /** Link to edit the file on GitHub */
 export const edit = (inputPath: string): string =>
-   `${SiteConfig.repo}/edit/main/${corrected(inputPath)}`;
+   `${SiteConfig.repo}/edit/main/${renderable(inputPath)}`;
 
 /** Link to view the file on GitHub */
 export const source = (inputPath: string): string =>
-   `${SiteConfig.repo}/blob/main/${corrected(inputPath)}`;
+   `${SiteConfig.repo}/blob/main/${renderable(inputPath)}`;
