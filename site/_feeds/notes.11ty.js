73eed2f98647cb51ae5d2c188dc5b1e36ed2fbf0
Chris Krycho <hello@chriskrycho.com>
2022-01-30 20:51:29 -0700

    Remove/update feeds.

--- site/_feeds/notes.11ty.js
+++
@@ -1,6 +0,0 @@
-import JSONFeed from '../../eleventy/feed';
-
-module.exports = class NotesFeed extends JSONFeed {
-   collection = 'notes';
-   title = 'Notes';
-};
--- site/_feeds/without-notes.11ty.js
+++
@@ -1,7 +0,0 @@
-import JSONFeed from '../../eleventy/feed';
-
-module.exports = class FeedWithoutNotes extends JSONFeed {
-   collection = 'nonNotes';
-   includeReplyViaEmail = false;
-   permalink = '/feeds/without-notes.json';
-};
