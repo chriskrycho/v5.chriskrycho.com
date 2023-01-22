f0d91e515d9025f097487848db5b2026dd52a60e
Chris Krycho <hello@chriskrycho.com>
2023-01-22 09:54:08 -0700

    Generalize `note` shortcode instead of special-casing
    `{% note type='Hypothesis' %}` 🎉

--- eleventy/notes.ts
+++ eleventy/notes.ts
@@ -1,14 +1,9 @@
 import markdown from './markdown';
 
-export const note = (content: string): string =>
-   `<section class='note' aria-label='note' aria-role='note'>${markdown.render(
+export const note = (content: string, { type = 'Note' } = {}): string =>
+   `<section class='note' aria-label='${type}' aria-role='note'>${markdown.render(
       content,
    )}</section>`;
 
 export const callout = (content: string): string =>
    `<section class='callout' aria-role='note'>${markdown.render(content)}</section>`;
-
-export const hypothesis = (content: string): string =>
-   `<section class='note' aria-label='note' aria-role='note'>${markdown.render(
-      '**Hypothesis:** ' + content,
-   )}</section>`;
