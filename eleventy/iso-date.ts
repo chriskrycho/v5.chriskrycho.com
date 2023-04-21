649bb92eb3a97adfb83b78dfdc28400e4f4b7580
Chris Krycho <hello@chriskrycho.com>
2023-04-21 13:53:00 -0600

    Improve error reporting for `isoDate` filter

--- eleventy/iso-date.ts
+++ eleventy/iso-date.ts
@@ -1,6 +1,15 @@
 import { fromDateOrString } from './date-time';
 
-const isoDate = (date: Date | string): string =>
-   fromDateOrString(date).toISO({ includeOffset: true });
+const isoDate = (date: Date | string): string => {
+   if (!date) {
+      throw new Error(`Could not parse '${date}' as date`);
+   }
+   let dateString = fromDateOrString(date).toISO({ includeOffset: true });
+   if (!dateString) {
+      throw new Error(`Could not convert '${date}' to date string`);
+   }
+
+   return dateString;
+};
 
 export default isoDate;
