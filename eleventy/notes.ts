e8dcde7b57fde69f3ee2b74d66d77f545c3ee3b9
Chris Krycho <hello@chriskrycho.com>
2023-10-05 16:30:30 -0600

    Add quote shortcode

--- eleventy/notes.ts
+++
@@ -1,9 +0,0 @@
-import markdown from './markdown';
-
-export const note = (content: string, { type = 'Note' } = {}): string =>
-   `<section class='note' aria-label='${type}' aria-role='note'>${markdown.render(
-      content,
-   )}</section>`;
-
-export const callout = (content: string): string =>
-   `<section class='callout' aria-role='note'>${markdown.render(content)}</section>`;
