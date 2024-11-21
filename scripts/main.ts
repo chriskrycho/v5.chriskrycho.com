6777f5e7a307e81cac8ab9bb9670a27c3f3996fc
Chris Krycho <hello@chriskrycho.com>
2024-11-21 11:13:00 -0700

    scripts: inline all functionality into main.ts
    Old habits about tiny modules really are not relevant here.

--- scripts/main.ts
+++ scripts/main.ts
@@ -1,25 +1,23 @@
-import { assert, unreachable } from './utils.js';
-import * as Preferences from './preferences.js';
-import getElements from './elements.js';
-
-const enum State {
-   Show,
-   Hide,
-}
+main();
 
 function main() {
    const { Root, Container, Panel, Show, Close, ColorSchemes, NavMode } = getElements();
 
    // At initialization, make sure the initial values are set correctly.
-   let { theme = Preferences.Theme.System, hideNav = false } = Preferences.load();
-   Preferences.persistTheme(theme, Root);
-   Preferences.persistNavMode(hideNav, Root);
+   let { theme = Theme.System, hideNav = false } = load();
+   persistTheme(theme, Root);
+   persistNavMode(hideNav, Root);
    ColorSchemes.forEach((el) => {
       if (el.id === theme) {
          el.checked = true;
       }
    });
    NavMode.checked = hideNav;
+
+   const enum State {
+      Show,
+      Hide,
+   }
 
    function setPanelTo(state: State) {
       switch (state) {
@@ -61,15 +59,141 @@ function main() {
    ColorSchemes.forEach((colorSchemeEl) => {
       colorSchemeEl.addEventListener('change', ({ target }) => {
          assert(target instanceof HTMLInputElement, 'misconfigured color scheme input');
-         assert(Preferences.isTheme(target.value), 'misconfigured color scheme value');
-         Preferences.persistTheme(target.value, Root);
+         assert(isTheme(target.value), 'misconfigured color scheme value');
+         persistTheme(target.value, Root);
       });
    });
 
    NavMode.addEventListener('change', ({ target }) => {
       assert(target instanceof HTMLInputElement, 'misconfigured reading theme input');
-      Preferences.persistNavMode(target.checked, Root);
+      persistNavMode(target.checked, Root);
    });
 }
 
-main();
+function load(): {
+   theme: Theme;
+   hideNav: boolean;
+} {
+   const themeFromStorage = localStorage.getItem(LocalStorage.Theme);
+   const theme =
+      themeFromStorage && isTheme(themeFromStorage) ? themeFromStorage : Theme.System;
+
+   const hideNav = localStorage.getItem(LocalStorage.NavMode) === 'hide';
+
+   return { theme, hideNav };
+}
+
+function persistTheme(newTheme: Theme, onElement: HTMLElement) {
+   updateThemeClass(newTheme, onElement);
+   saveThemePreference(newTheme);
+}
+
+function persistNavMode(inReadingMode: boolean, onElement: HTMLElement) {
+   updateReadingModeClass(inReadingMode, onElement);
+   saveReadingModePreference(inReadingMode);
+}
+
+/**
+   Update the style class on the root (`html`) element.
+
+   @param newTheme The new theme to set on that `html` element.
+   @param element The target element to set the class on.
+ */
+const updateThemeClass = (newTheme: Theme, element: HTMLElement): void => {
+   THEME_VALUES.forEach((className) => element.classList.remove(className));
+   if (newTheme !== Theme.System) element.classList.add(newTheme);
+};
+
+function updateReadingModeClass(inReadingMode: boolean, element: HTMLElement): void {
+   if (inReadingMode) {
+      element.classList.add('hide-side-nav');
+   } else {
+      element.classList.remove('hide-side-nav');
+   }
+}
+
+const enum LocalStorage {
+   Theme = 'sympolymathesy:theme',
+   NavMode = 'sympolymathesy:hide-side-nav',
+}
+
+// If the user chooses to follow the OS, simply delete the key from local storage: there
+// is no need to *store* "use the OS".
+function saveThemePreference(theme: Theme): void {
+   if (theme !== Theme.System) {
+      localStorage.setItem(LocalStorage.Theme, theme);
+   } else {
+      localStorage.removeItem(LocalStorage.Theme);
+   }
+}
+
+function saveReadingModePreference(hideNav: boolean): void {
+   if (hideNav) {
+      localStorage.setItem(LocalStorage.NavMode, 'hide');
+   } else {
+      localStorage.removeItem(LocalStorage.NavMode);
+   }
+}
+
+// SAFETY: we immediately check (in debug!) that each cast is correct.
+export default function getElements() {
+   const Root = $('root');
+   assert(Root instanceof HTMLElement, 'misconfigured root element');
+
+   const Container = $('preferences');
+   assert(Container instanceof HTMLElement, 'misconfigured preferences element');
+   Container.classList.remove('no-js-hidden');
+
+   const Panel = $('panel');
+   assert(Panel instanceof HTMLElement, 'misconfigured panel element');
+
+   const Show = $('show');
+   assert(Show instanceof HTMLButtonElement, 'misconfigured adjust element');
+
+   const Close = $('close');
+   assert(Close instanceof HTMLButtonElement, 'misconfigured adjust element');
+
+   const ColorSchemes = $$('theme');
+   assert(
+      ColorSchemes.length === 3 &&
+         ((cs: typeof ColorSchemes): cs is NodeListOf<HTMLInputElement> =>
+            Array.from(cs).every(
+               (scheme) => scheme instanceof HTMLInputElement && scheme.type === 'radio',
+            ))(ColorSchemes),
+      'misconfigured color schemes',
+   );
+
+   const NavMode = $('hide-side-nav');
+   assert(
+      NavMode instanceof HTMLInputElement && NavMode.type === 'checkbox',
+      'misconfigured reading mode',
+   );
+
+   return { Root, Container, Panel, Show, Close, ColorSchemes, NavMode };
+}
+
+function assert(pred: unknown, message: string): asserts pred {
+   if (!pred) {
+      throw new Error(message);
+   }
+}
+
+function unreachable(value: never): never {
+   throw new Error(`Should have been unreachable with ${value}`);
+}
+
+const selectorFor = (item: string): string => `[data-sympolymathesy="${item}"]`;
+export const $ = (item: string) => document.querySelector(selectorFor(item));
+export const $$ = (item: string) => document.querySelectorAll(selectorFor(item));
+
+/** Defines the class names for the theme */
+const enum Theme {
+   System = 'system',
+   Light = 'light',
+   Dark = 'dark',
+}
+
+// Intentionally throw away type checking here.
+const THEME_VALUES: string[] = [Theme.System, Theme.Light, Theme.Dark];
+
+const isTheme = (s: string): s is Theme => THEME_VALUES.includes(s);
